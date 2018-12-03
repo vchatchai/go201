bd3bbf1d22df434cdf08c519fc7561f8f3701046
vchatchai <ee56054@gmail.com>
2018-12-03 14:01:36 +0700

    update http session

---
+++ httpsession/http-session_test.go
@@ -0,0 +1,16 @@
+package httpsession
+
+import "testing"
+
+func TestHttpSession(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{"TestHttpSession"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			HttpSession()
+		})
+	}
+}
