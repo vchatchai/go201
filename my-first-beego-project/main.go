42881337c581dd99461a528e7218cc8d5af33963
vchatchai <ee56054@gmail.com>
2019-02-01 21:00:18 +0700

    add create session on bee

--- my-first-beego-project/main.go
+++ my-first-beego-project/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego/session/redis"
 	_ "github.com/vchatchai/go201/my-first-beego-project/routers"
 )
 
 func main() {
+	beego.BConfig.WebConfig.DirectoryIndex = true
+	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	beego.Run()
 }
--- my-first-beego-project/routers/router.go
+++ my-first-beego-project/routers/router.go
@@ -9,4 +9,7 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/employees", &controllers.FirstController{}, "get:GetEmployees")
 	beego.Router("/dashboard", &controllers.FirstController{}, "get:Dashboard")
+	beego.Router("/home", &controllers.SessionController{}, "get:Home")
+	beego.Router("/login", &controllers.SessionController{}, "get:Login")
+	beego.Router("/logout", &controllers.SessionController{}, "get:Logout")
 }
